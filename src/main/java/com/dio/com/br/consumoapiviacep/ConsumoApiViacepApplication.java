7d93ef4f18c582972182a99a06d35ed159dc9fe8
Fernando de Oliveira Carvalho <fernandooliveiracarvalho98@gmail.com>
2022-08-05 15:44:28 -0300

    criando repositório

---
+++ src/main/java/com/dio/com/br/consumoapiviacep/ConsumoApiViacepApplication.java
@@ -0,0 +1,13 @@
+package com.dio.com.br.consumoapiviacep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumoApiViacepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumoApiViacepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/com/br/consumoapiviacep/ConsumoApiViacepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.com.br.consumoapiviacep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumoApiViacepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
